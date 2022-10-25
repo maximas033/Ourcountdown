e555c6eb6aef43cc11b0a7f8dedcfe7e1eb10c0d
maximas033 <101352756+maximas033@users.noreply.github.com>
2022-10-25 13:47:54 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,63 +1,29 @@
-// function that counts down from 5 to 0 and then gives a random dirrection either left or right 
-// after counting down from 5 to 0 automatically calls countDown2 function that will count down from 10 to 0 and then give a random direction either left or right, countdown from 10 to 0 will forever repeat
-// print the countdown to the console
+// create a countdown to September 2, 2023
+function countdown() {
+  const countDate = new Date('September 2, 2023 00:00:00').getTime();
+  const now = new Date().getTime();
+  const gap = countDate - now;
 
-function countDown1() {
-    var count = 5;
-    var interval = setInterval(function() {
-        if (count == 0) {
-            const dirr = ["Left", "Right"];
-            const random = Math.floor(Math.random() * dirr.length);
-            document.getElementById("dirrection").innerHTML = dirr[random];
-            // speak the dirrection
-            var msg = new SpeechSynthesisUtterance(dirr[random]);
-            window.speechSynthesis.speak(msg);
-            clearInterval(interval);
-            countDown2();
-            setTimeout(function() {
-                document.getElementById("dirrection").innerHTML = "";
-            }
-            , 1000);
-        }
-        document.getElementById("num").innerHTML = count;
-        count--;
-    }, 1000);
+  // how does time work?
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // calculate
+  const textDay = Math.floor(gap / day);
+  const textHour = Math.floor((gap % day) / hour);
+  const textMinute = Math.floor((gap % hour) / minute);
+  const textSecond = Math.floor((gap % minute) / second);
+
+  // create a countdown that is refreshed every second
+    console.log(textDay, textHour, textMinute, textSecond);
+
+    document.getElementById("WeddingDay").innerHTML = "Days: " + " " + textDay;
+    document.getElementById("WeddingHours").innerHTML = "Hours: " + " " + textHour;
+    document.getElementById("WeddingMinute").innerHTML = "Minutes: " + " " + textMinute;
+    document.getElementById("WeddingSecond").innerHTML = "Seconds: " + " " + textSecond;
 }
 
-function countDown2() {
-    var count = 10;
-    var interval = setInterval(function() {
-        if (count == 0) {
-            // print random direction either left or right to the dirrection div
-            const dirr = ["Left", "Right"];
-            const random = Math.floor(Math.random() * dirr.length);
-            document.getElementById("dirrection").innerHTML = dirr[random];
-            // speak the dirrection
-            var msg = new SpeechSynthesisUtterance(dirr[random]);
-            window.speechSynthesis.speak(msg);
-            clearInterval(interval);
-            countDown2();
-            // clear the dirrection div after 1 seconds
-            setTimeout(function() {
-                document.getElementById("dirrection").innerHTML = "";
-            }
-            , 1000);
-        }
-        document.getElementById("num").innerHTML = count;
-        count--;
-    }, 1000);
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+countdown();
+setInterval(countdown, 1000);
